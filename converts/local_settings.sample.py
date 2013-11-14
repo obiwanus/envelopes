cfbdb624342f9318365531e0b5e979dfb1a049ef
Ivan Ivanov <ivan@Ivan-PC.ivanovs.info>
2013-11-14 20:26:46 +1300

    Django 1.6 and settings

---
+++ converts/local_settings.sample.py
@@ -0,0 +1,8 @@
+# Create a file local_settings.py using this as a template
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/.../converts.sqlite3',
+    }
+}
