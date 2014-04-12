f341f0b885f5391d5a3c543d5c75ebc897c6646d
Ivan Ivanov <ivan@ivanovs.info>
2014-04-12 20:00:52 +1200

    Create default settings for new users

--- converts/models.py
+++ converts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db import models
 
 import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 PERIODICITY_CHOICES = (
@@ -22,6 +24,13 @@ class Settings(models.Model):
         default='f',
         choices=(('w', 'Неделя'), ('f', '2 недели'), ('m', 'Месяц')),
     )
+
+
+@receiver(post_save, sender=User)
+def create_settings(sender, instance, created=False, **kwargs):
+    """ Creates default settings for a user """
+    if created:
+        Settings.objects.create(user=instance)
 
 
 class Income(models.Model):
--- converts/tags.py
+++
@@ -1,9 +0,0 @@
-from distutils.command import register
-
-
-@register.simple_tag
-def active(request, pattern):
-    import re
-    if re.search(pattern, request.path):
-        return 'active'
-    return ''
