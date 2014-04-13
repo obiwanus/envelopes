174a81d97b458945d388997af6d74999d46d936f
Ivan Ivanov <ivan@ivanovs.info>
2014-04-13 17:23:19 +1200

    Register all models in admin

--- converts/admin.py
+++ converts/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from converts.models import Income
+from converts.models import Income, Expense, Goal, ActualExpense, Settings
 
 
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_date', 'size', 'periodicity')
 
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'size', 'periodicity')
+
+
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'saved', 'payment_size', 'payment_periodicity')
+
+
+class ActualExpenseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'size', 'date', 'regular_expense')
+
+
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'start_date', 'period_length')
+
+
 admin.site.register(Income, IncomeAdmin)
+admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(Goal, GoalAdmin)
+admin.site.register(ActualExpense, ActualExpenseAdmin)
+admin.site.register(Settings, SettingsAdmin)
